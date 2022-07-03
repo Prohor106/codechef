c69bb0594936a47081ed7458854f46f6af0a17ae
Prohor Paul <prohorpaul121@gmail.com>
2022-07-03 18:50:19 +0600

    Create Fitness.cpp

---
+++ Fitness.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int no_testcase;
+    int x;
+    cin >> no_testcase;
+    for (int i = 0; i < no_testcase; i++)
+    {
+        cin >> x;
+        x = x * 2 * 5;
+        cout << x << endl;
+    }
+}
